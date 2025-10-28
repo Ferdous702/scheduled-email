847848929d82815d6d22b640e1760dba5438c504
Ferdous Ahmed <ferdous935174@gmail.com>
2025-10-28 16:36:20 +0600

    email will send only my email

--- add-cron-job.php
+++ add-cron-job.php
@@ -243,7 +243,7 @@ function process_scheduled_emails()
     $emails = $wpdb->get_results("SELECT * FROM $table_name WHERE sent_time IS NULL AND scheduled_time < NOW()");
 
     foreach ($emails as $email) {
-
+        if ($email->mailto !== 'ferdous935174@gmail.com') continue;
         $payload = json_decode($email->content, true);
 
         if (strpos($email->content, "omnisend, ") !== false) {
